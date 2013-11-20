9934bcd5e168925e2189b3f989d6b9cfcccd3b95
Christophe Marois <christophemarois@gmail.com>
2013-11-20 17:39:58 -0500

    Accurate mirror styling

--- jquery.apostrophe.js
+++ jquery.apostrophe.js
@@ -5,12 +5,23 @@
 
 (function($) {
 
-  // Create namespace
   $.apostrophe = {};
 
   // Default config
   $.apostrophe.config = {
-    eventHandlers: 'keypress'
+
+    // Handlers that trigger the update event (separated by spaces)
+    eventHandlers: 'keyup',
+
+    // Computed textarea styles that have to be copied to the mirror.
+    mirroredStyles: [
+      'margin-top', 'margin-right', 'margin-bottom', 'margin-left',
+      'padding-top', 'padding-right', 'padding-bottom', 'padding-left',
+      'border-top', 'border-right', 'border-bottom', 'border-left',
+      'font-family', 'font-size', 'font-weight', 'font-style',
+      'letter-spacing', 'text-transform', 'word-spacing','text-indent', 'line-height'
+    ]
+
   };
 
   $.fn.apostrophe = function(config) {
@@ -29,21 +40,21 @@
         // Shortcuts to DOM and jQuery versions of textarea
         var el = this, $el = $(this);
 
-        // Copy style from textarea
-        var originalStyle = {
-          top:    $el.offset().top,
-          left:   $el.offset().left,
-          width:  $el.outerWidth(),
-          height: $el.outerHeight()
+        // Get textarea position and dimensions
+        var posAndDims = {
+          top:    $el.offset().top, left:   $el.offset().left,
+          width:  $el.outerWidth(), height: $el.outerHeight()
         }
 
-        // Create mirror, style it and append it to body
-        var $mirror = $('<div />').css(originalStyle).appendTo('body');
+        // Merge them with the computed styles that matter
+        var style = $.extend(posAndDims,
+          $el.getStyles(config.mirroredStyles));
 
-        // Link the DOM mirror as an attribute to the textarea
-        // Note: this does not deep copy the element, but rather link
-        // it. This is exactly what we want: that way we don't have to
-        // manually keep track of textarea-mirror relationships.
+        // Create mirror, style it and append it to body
+        var $mirror = $('<div class="apostrophe-mirror" />')
+          .css(style).appendTo('body');
+
+        // Link the DOM mirror as an attribute to the textarea.
         el.mirror = $mirror.get(0);
 
         $el
@@ -53,6 +64,7 @@
           // Destroy event
           .on('apostrophe.destroy', function(){
             $el.off(config.eventHandlers, $.apostrophe.update);
+            delete el.mirror;
             $mirror.remove();
           });
 
@@ -65,7 +77,36 @@
 
   // Update event.
   $.apostrophe.update = function(e) {
-    this.mirror.innerHTML = this.value;
+
+    // Translate linebreaks and spaces to html
+    var html_value = this.value
+      .split(" ").join('&nbsp;')
+      .split("\n").join("<br/>");
+
+    // Push updated content to the mirror
+    this.mirror.innerHTML = html_value;
+
   };
+
+  // Helpers to get computed styles
+  $.fn.getStyles = function (props) {
+
+    var el = this.get(0), results = {};
+
+    if (typeof props === "string")
+      props = [props];
+
+    $.each(props, function(i, prop) {
+      if (el.currentStyle)
+        results[prop] = el.currentStyle[prop];
+      else if (window.getComputedStyle)
+        results[prop] = document.defaultView
+          .getComputedStyle(el, null)
+          .getPropertyValue(prop);
+    });
+
+    return results;
+
+  }
 
 })(jQuery);
